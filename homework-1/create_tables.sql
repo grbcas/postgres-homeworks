9baa3f4fa21ec81c247678324ab29c2aa674c612
grbcas <grbcas@gmail.com>
2023-07-09 03:23:48 +0300

    hw#1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,35 @@
--- SQL-команды для создания таблиц
+drop table if exists public.employees;
+drop table if exists public.customers;
+drop table if exists public.orders;
+
+
+create table public.employees
+(
+    employee_id smallint PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(30),
+	birth_day date NOT NULL,
+		check ( birth_day < (current_date - interval '18' year ) ),
+	notes text
+);
+
+create table public.customers
+(
+	customer_id varchar(5) PRIMARY key,
+	company_name varchar(50) NOT null,
+	contact_name varchar(100)
+);
+
+create table public.orders
+(
+	order_id int PRIMARY key,
+	customer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id smallint REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
+
+select * from public.customers;
+select * from public.employees;
+select * from public.orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,43 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+from pathlib import Path
+
+employees_data_csv_path = Path(Path(__file__).parent, 'north_data', 'employees_data.csv')
+customers_data_csv_path = Path(Path(__file__).parent, 'north_data', 'customers_data.csv')
+orders_data_csv_path = Path(Path(__file__).parent, 'north_data', 'orders_data.csv')
+
+print()
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="pgpwd1234"
+)
+
+with conn.cursor() as cur:
+
+    copy_from = """
+    COPY employees 
+    FROM STDIN
+    WITH (
+        FORMAT CSV,
+        DELIMITER ',',
+        HEADER
+    );
+    """
+
+    with open(employees_data_csv_path, 'r') as f_csv:
+        # next(f_csv)
+        cur.copy_expert(copy_from, f_csv)
+        conn.commit()
+
+    with open(customers_data_csv_path) as f_csv:
+        next(f_csv)
+        cur.copy_from(f_csv, 'customers', sep=',')
+        conn.commit()
+
+    with open(orders_data_csv_path) as f_csv:
+        next(f_csv)
+        cur.copy_from(f_csv, 'orders', sep=',')
+        conn.commit()
